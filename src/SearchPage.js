4bc96701ccf317fe6eacc34126f4ed2a961f2237
Ansuman09 <ansumansamal1@gmail.com>
2024-10-26 12:14:02 +0530

    added post based search

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -12,8 +12,10 @@ const SearchPage=()=>{
         const nav = useNavigate();
         const [userLoading,setUserLoading]=useState(true);
         const [searchType,setSearchType]=useState("users");
+
+        const [postLoading,setPostLoading]=useState(true);
+        const [postData,setPostData]=useState([]);
         const token = localStorage.getItem('token');
-        const [selectedSearch,setSelectedSearch]=useState("users");
                         
 
         const fetchImageUrl = async (imageName) => {
@@ -93,6 +95,72 @@ const SearchPage=()=>{
             const handleNameClick=(username)=>{
                 nav(`/userprofile/${username}`);
             }
+        const handleRunQueryOnPosts = async () => {
+            
+            const fetchPostsData = async () => {
+                try {
+                    const response = await fetch(`http://localhost:8080/posts/home/search/${q}`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-type': 'application/json',
+                            Authorization: `Bearer ${token}`
+                        }
+                    });
+
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+
+                    const data = await response.json();
+                    setPostData(data);
+                    console.log(data);
+                    setSearchType("posts");
+
+                    //fetchPostImageafter posts data
+                    await fetchPostWithImage(data); 
+
+                } catch (error) {
+                    console.log(error);
+                    setPostLoading(false); 
+                    setPostLoading(true);
+                }
+            };
+
+            const fetchPostWithImage = async (posts) => {
+                setPostLoading(true); 
+                const postDataWithImageUrl = await Promise.all(
+                    posts.map(async (post) => {
+                        const imageUrl = await fetchImageUrl(post.image.imageName);
+                        return { ...post, imageUrl }; 
+                    })
+                );
+
+                setPostData(postDataWithImageUrl);
+
+                setPostLoading(false);
+                
+            };
+
+            fetchPostsData();
+        };
+
+        // useEffect(()=>{
+        //         const updatePosts=async()=>{
+        //             const userDataWithImageUrl=await Promise.all(
+        //                 posts.map(async (post)=>{
+        //                     const imageUrl=await fetchImageUrl(post.image.);
+        //                     return {...user,imageUrl};
+        //                 })
+        //             )
+                    
+        //             setUsers(userDataWithImageUrl);
+        //             console.log(users);
+        //         }
+
+        //         updateUsers();
+        //         setLoading(false);
+                
+        //     },[userLoading]);
 
             if (loading) {
                 return <h2>Loading...</h2>;
@@ -103,8 +171,8 @@ const SearchPage=()=>{
                     
                     <div className="search-result-container">
                     <div className="searchtype-container">
-                        <button className={selectedSearch==="users" ? "selected-button":"unselected-button"}>Users</button>
-                        <button className={selectedSearch==="posts" ? "selected-button":"unselected-button"}>Posts</button>
+                        <button className={searchType==="users" ? "selected-button":"unselected-button"} onClick={()=>{setSearchType("users"),setPostLoading(true)}}>Users</button>
+                        <button className={searchType==="posts" ? "selected-button":"unselected-button"} onClick={()=>{setSearchType("posts"),handleRunQueryOnPosts(),setUserLoading(true)}}>Posts</button>
                         </div>
                 
                         {searchType=="users" && users.map((user) => (
@@ -113,6 +181,14 @@ const SearchPage=()=>{
                                 <h3>{user.username}</h3>
                             </div>    
                         ))}
+                        {searchType=="posts" && !postLoading&& 
+                        <div className="search-view-posts-container">
+                        {postData.map(post=>(
+                        <div className="search-view-post-container ">
+                            <img src={post.imageUrl}/>
+                        </div>))}
+                        </div>
+                    }
                     </div>
                     </div>
                 );
