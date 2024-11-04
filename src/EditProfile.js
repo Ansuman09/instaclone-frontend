b9e574f529b95f68a2aabf1e52433f890e22a904
Ansuman09 <ansumansamal1@gmail.com>
2024-11-04 07:08:32 +0530

    create-user-profile-editor

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -1,8 +1,82 @@
 import React from "react";
 import NavBar from "./NavBar";
-
+import { useState,useEffect } from "react";
+import "./EditProfile.css";
 
 const editProfile=()=>{
+
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const token = localStorage.getItem('token');
+    const [user,setUser] = useState({});
+    const [userProfileImage,setUserProfileImage]=useState();
+    const [changeProfileImage,setChangeProfileImage]=useState(false);
+    const [userDataToSend,setUserDataToSend]=useState({});
+
+    const fetchImageUrl = async (imageName) => {
+        console.log("Called image data");
+        try {
+          const response = await fetch(`${apiUrl}/get-images/images/${imageName}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          });
+      
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+      
+          const imageBlob = await response.blob();
+          const imageUrl = URL.createObjectURL(imageBlob);
+          console.log(`Got image data: ${imageUrl}`);
+          return imageUrl;
+          // Use imageUrl to display the image, e.g., set it in an <img> element.
+        } catch (error) {
+          console.log("Unable to get image:", error);
+        }
+      };
+    
+    useEffect(()=>{
+        const fetchUserInfo= async()=> {
+          try {
+          const response=await fetch(`${apiUrl}/userinfo/image`,{
+            method:"GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`
+            }
+  
+          });
+  
+          const userData=await response.json();
+          setUser(userData);
+          setUserDataToSend(userData);
+          console.log(userData);
+        }catch(error){
+          console.log(error);
+        } }
+  
+        fetchUserInfo();
+      },[])
+  
+    useEffect(()=>{
+        if (!user || !user.profile_image) return;
+        const getUserProfileImage= async ()=>{
+          const ProfileImageUrl=await fetchImageUrl(user.profile_image);
+          setUserProfileImage(ProfileImageUrl)
+        }
+        getUserProfileImage()
+      },[user])
+  
+    const handleFileUpload=(imageFile)=>{
+        const image=URL.createObjectURL(imageFile);
+        console.log(imageFile.name);
+        setUserProfileImage(image);
+        setUserDataToSend({...userDataToSend,profile_image:imageFile.name})    
+        console.log(userDataToSend);
+    }
+
     return(
     <div>
          <div style={{'padding-bottom':'120px'}}>
@@ -10,6 +84,14 @@ const editProfile=()=>{
               
         </div>
         <div style={{paddingLeft:300}}>
+        <form className="edit-profile-form">
+            {!changeProfileImage && <button type="button" onClick={()=>{setChangeProfileImage(true)}}>Change Profile Pic</button>}
+            {changeProfileImage && <input type="file" onChange={(e)=>handleFileUpload(e.target.files[0])} />}
+            <p></p>
+            <img src={userProfileImage} alt="Profile image"></img>
+            <p>Username: </p>
+            <input value={userDataToSend.username} onChange={(e)=>setUserDataToSend({...userDataToSend,username:e.target.value})}></input>
+        </form>
         <p>edit profile here</p>
         </div>
     </div>)
