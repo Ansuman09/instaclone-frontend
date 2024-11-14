23c95290fa1354fb2ea91363e95ff396bf554b8a
Ansuman09 <ansumansamal1@gmail.com>
2024-11-14 07:55:44 +0530

    admin-controller

--- src/AdminController.js
+++ src/AdminController.js
@@ -1,9 +1,14 @@
+import { faAngleRight, faDeleteLeft, faEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
+import NavBar from "./NavBar";
+import "./AdminController.css";
+
 const AdminController =()=>{
 
     const [table,setTable]=useState();
-    const [posts,setPosts]=useState();
-    const [actions,setActions]=useState();
+    const [posts,setPosts]=useState({});
+    const [actions,setActions]=useState({});
     const [loading,setLoading]=useState(true);
 
     const token=localStorage.getItem('token');
@@ -28,41 +33,93 @@ const AdminController =()=>{
                     const data = await response.json()
                     setPosts(data);
                     console.log(data)
-
+                    setLoading(false)
                 }
                 
             }
 
-            getPostsData().then(setLoading(false));
+            getPostsData()  ;
         }
         console.log(table)
+
+        if (table=="actions"){
+            const getActionData=async ()=>{
+                const response = await fetch(`${apiUrl}/action/all`,{
+                    method:"GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                      }
+
+                })
+
+                if (response.ok){
+                    const data = await response.json()
+                    setActions(data);
+                    console.log(data)
+                    setLoading(false)
+                }
+                
+            }
+
+            getActionData()  ;
+        }
     }
 
     return(
         <div>
+            <NavBar />        
+        <div style={{paddingLeft:300,paddingTop:150}}>
+            
             <h3>This is where sm manager works</h3>
 
             <form>
                 <label>
                     Select Database
-                    <select onChange={(e)=>setTable(e.target.value)}>
+                    <select onChange={(e)=>{setTable(e.target.value),setLoading(true)}}>
                         <option value="posts">Posts</option>
                         <option value="actions">Actions</option>
                     </select>
-                    <button type="submit" onClick={(e)=>handleFetchTableData(e)}>Go</button>
+                    <button type="submit" onClick={(e)=>handleFetchTableData(e)} style={{marginLeft:3,cursor:"pointer",backgroundColor:"green",color:"white"}}>   <FontAwesomeIcon icon={faAngleRight}/></button>
                 </label>
             </form>
-            {!loading && <table>
+            <div className="data-table-container">
+            {!loading && table==="posts" &&<table className="data-table">
                 <th>post_id</th>
                 <th>owner_id</th>
                 <th>description</th>
+                <th>Delete </th>
+                <th>Update </th>
                 {posts.map(post=>(
                             <tr>
                             <td>{post.post_id}</td>
                             <td>{post.owner_id}</td>
                             <td>{post.description}</td>
+                            <td><button type="button" className="delete-btn"><FontAwesomeIcon icon={faDeleteLeft}/> </button></td>
+                            <td><button type="button" className="update-btn"><FontAwesomeIcon icon={faEdit}/> </button></td>
                             </tr>))}
             </table>}
+            {!loading && table==="actions" && 
+                <table className="data-table">
+                    <th>post_id</th>
+                    <th>user_id</th>
+                    <th>action</th>
+                    <th>Delete </th>
+                    <th>Update </th>
+                    {actions.map(action=>(
+                        <tr>
+                            <td>{action.post_id}</td>
+                            <td>{action.user_id}</td>
+                            <td>{action.action}</td>
+                            <td><button type="button" className="delete-btn"><FontAwesomeIcon icon={faDeleteLeft}/> </button></td>
+                            <td><button type="button" className="update-btn"><FontAwesomeIcon icon={faEdit}/> </button></td>
+                            
+                        </tr>
+                    ))}
+                </table>
+            }
+            </div>
+        </div>
         </div>
     )
 }
